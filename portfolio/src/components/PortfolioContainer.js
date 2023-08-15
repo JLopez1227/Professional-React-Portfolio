008f7a6663afdd12f62d666d197d0e3a1f39e71c
JLopez1227 <josuenmilopez27@gmail.com>
2023-08-15 14:24:04 -0500

    Fixed errors, added header and footer components. Have to work on stlying and adding information.

--- portfolio/src/components/PortfolioContainer.js
+++ portfolio/src/components/PortfolioContainer.js
@@ -1,23 +1,25 @@
 import React, { useState } from 'react';
 import NavTabs from './NavTabs';
-import Home from './pages/Home';
-import About from './pages/About';
-import Blog from './pages/Blog';
+import Resume from './pages/Resume';
+import About from './pages/AboutMe';
+import Portfolio from './pages/Portfolio';
 import Contact from './pages/Contact';
+import Header from './Header';
+import Footer from './Footer';
 
 export default function PortfolioContainer() {
-  const [currentPage, setCurrentPage] = useState('Home');
+  const [currentPage, setCurrentPage] = useState('Resume');
 
   // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
   const renderPage = () => {
-    if (currentPage === 'Home') {
-      return <Home />;
+    if (currentPage === 'Resume') {
+      return <Resume />;
     }
     if (currentPage === 'About') {
       return <About />;
     }
-    if (currentPage === 'Blog') {
-      return <Blog />;
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />;
     }
     return <Contact />;
   };
@@ -26,10 +28,12 @@ export default function PortfolioContainer() {
 
   return (
     <div>
+      <Header />
       {/* We are passing the currentPage from state and the function to update it */}
       <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
       {/* Here we are calling the renderPage method which will return a component  */}
       {renderPage()}
+      <Footer />
     </div>
   );
 }
